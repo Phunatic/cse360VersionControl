7a11155565d58b6103478323172ebaee9d09dfe5
Matt3 <Matt3@LAPTOP-ACGGUO48>
2020-10-05 19:07:45 -0700

    2nd version completing the methods

--- Assignment01/src/cse360assignment02/AddingMachine.java
+++ Assignment01/src/cse360assignment02/AddingMachine.java
@@ -1,28 +1,37 @@
-/**
 package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history = "0"; // holds the history starting at "0"
+  private String operator = ""; // holds the individual operation to be appended to history
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
-  public int getTotal () {
-    return 0;
+  public int getTotal () { // returns current total
+    return total;
   }
   
-  public void add (int value) {
+  public void add (int value) { // adds parameter to total value
+	  total += value;
+	  operator = " + " + value; 
+	  history += operator;
   }
 
-  public void subtract (int value) {
+  public void subtract (int value) { // subtracts parameter from total value
+	  total -= value;
+	  operator = " - " + value;
+	  history += operator;
   }
 
-  public String toString () {
-    return "";
+  public String toString () { // returns the history of transactions
+    return history;
   }
 
-  public void clear() {
+  public void clear() { // clears the "memory"
+	  history = "0";
+	  total = 0;
   }
+
 }
-*/
